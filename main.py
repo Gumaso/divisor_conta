ecc13f1018961ff8209db577482756e4ec5f2c29
Gustavo <gustavomacedosousa013@gmail.com>
2023-08-01 17:11:52 -0300

    Fazendo o calculo da conta e atualizando os valores

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ else:
         nome = input(f"Qual o nome do participante № {amg + 1}: ")
         item_dicionario = {nome: 0}
         amigos.update(item_dicionario)
+
+valor_conta = float(input("Qual o valor da conta total: R$"))
+valor_a_ser_pago_individual = round(valor_conta / len(amigos), 2)
+for nome in amigos:
+    amigos[nome] = valor_a_ser_pago_individual
 print(f"Lista de participantes:")
 for c, v in amigos.items():
     print(f"Nome: {c} | Conta: R${v}")
